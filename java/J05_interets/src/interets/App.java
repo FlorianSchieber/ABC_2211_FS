fdf2ed75293cb6ddacabafdb08f1ce355546d723
FlorianSchieber <81633312+FlorianSchieber@users.noreply.github.com>
2022-12-21 10:03:18 +0100

    Peaufinage exo algo 1.4

--- java/J05_interets/src/interets/App.java
+++ java/J05_interets/src/interets/App.java
@@ -8,33 +8,41 @@ public class App {
 		
 		//VARIABLES
 
-		double S;
+		int S;
 		double i;
-		double N;
-		double interetSimple;
-		double interetCompose;
+		int N;
+		int interetSimple;
+		int beneficeSimple;
+		int interetCompose;
+		int beneficeCompose;
 		
 		//DEBUT PROGRAMME
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("PROGRAMME DE CALCUL D'INTÉRET");
+		System.out.println("PROGRAMME DE CALCUL D'INTERET");
 		
 		System.out.print("Veuillez saisir la somme placee sur votre compte : ");
-		S = sc.nextDouble();
+		S = (int) (sc.nextDouble() * 100);
 		
 		System.out.print("Veuillez saisir l'interet offert par la banque (en pourcentage) : ");
 		i = sc.nextDouble();
 		
 		System.out.print("Veuillez saisir le nombre d'annees de placement de la somme : ");
-		N = sc.nextDouble();
+		N = sc.nextInt();
 		
-		interetSimple = S * (1 + N * i / 100);
-		interetCompose = S * Math.pow((1 + i / 100), N);
+		interetSimple = (int) (S * (1 + N * i / 100));
+		interetCompose = (int) (S * Math.pow((1 + i / 100), N));
 		
-		System.out.println("La valeur acquise par la somme " + S + " placee pendant " + N + " annees est de : ");
-		System.out.println(interetSimple + " avec un interet simple");
-		System.out.println(interetCompose + " avec un interet compose");
+		beneficeSimple = interetSimple - S;
+		beneficeCompose = interetCompose - S;
+		
+		
+		System.out.println("La \"valeur\" acquise par la somme " + (double) S / 100 + " placee pendant " + N + " annees est de : ");
+		System.out.println((double) interetSimple / 100  + " avec un interet simple, soit un benefice de " + (double) beneficeSimple / 100);
+		System.out.println((double) interetCompose / 100 + " avec un interet compose, soit un benefice de " + (double) beneficeCompose / 100);
+		
+		sc.close();
 		
 	}
 
